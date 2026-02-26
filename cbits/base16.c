d2992d1f3a101d8dcb56d59a5e063b06fa6dd809
Sélène Corbineau <selene.corbineau@ens.fr>
2026-02-26 08:55:20 +0100

    Don't reference x86 symbols on all archs
    Also make symbols to vectorized functions default to nullptr
    whenever no suitable implementation is found.

--- cbits/base16.c
+++ cbits/base16.c
@@ -2,29 +2,44 @@
 
 static void (*resolve_encode (void)) (const uint8_t* restrict,size_t,uint8_t* restrict) {
 	__builtin_cpu_init();
+
+#ifdef __x86_64__
 	if(__builtin_cpu_supports("avx2")) {
 		return avx_encodeBase16;
-	} else {
+	} else if(__builtin_cpu_supports("sse4.1")) {
 		return sse_encodeBase16;
 	}
+#endif
+
+	return nullptr;
 }
 
 static void (*resolve_decode (void)) (const uint8_t* restrict,size_t,uint8_t* restrict) {
 	__builtin_cpu_init();
+
+#ifdef __x86_64__
 	if(__builtin_cpu_supports("avx2")) {
 		return avx_decodeBase16;
-	} else {
+	} else if(__builtin_cpu_supports("sse4.1")) {
 		return sse_decodeBase16;
 	}
+#endif
+
+	return nullptr;
 }
 
 static bool (*resolve_isvalid (void)) (const uint8_t* restrict,size_t){
 	__builtin_cpu_init();
+
+#ifdef __x86_64__
 	if(__builtin_cpu_supports("avx2")) {
 		return avx_isValidBase16;
-	} else {
+	} else if(__builtin_cpu_supports("sse4.1")) {
 		return sse_isValidBase16;
 	}
+#endif
+
+	return nullptr;
 }
 
 void encodeBase16(const uint8_t* restrict input, size_t len, uint8_t* restrict output)

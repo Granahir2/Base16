3b39104cd48f6ff5f3e455da2e64dc3e39f6c187
Hécate Kleidukos <hecate+github@glitchbra.in>
2026-05-02 01:44:05 +0200

    Enable benchmarks, tests and simd flag in their own command line

--- benchmarks/Base16Bench.hs
+++ benchmarks/Base16Bench.hs
@@ -14,8 +14,8 @@ import "base16" Data.ByteString.Base16 as B16
 import "base16" Data.ByteString.Short.Base16 as BS16
 import "base16-bytestring" Data.ByteString.Base16 as Bos
 import Data.ByteString.Random (random)
-import Data.ByteString.Base16.Internal.SIMD (c_isSIMDAvailable)
-import qualified Data.ByteString.Base16.Internal.SIMD as SIMD
+import Data.ByteString.Base16.Internal.Simd (c_isSIMDAvailable)
+import qualified Data.ByteString.Base16.Internal.Simd as Simd
 
 
 main :: IO ()
@@ -26,32 +26,32 @@ main =
       [ bgroup "25" [ bench "base16-short" $ whnf BS16.encodeBase16' bs25L
         , bench "base16-bytestring" $ whnf Bos.encode bs25
         , bench "base16 (scalar)" $ whnf (assertBase16 . B16.encodeBase16_) bs25
-        , bench "base16 (simd)" $ whnf SIMD.encodeBase16SIMD bs25
+        , bench "base16 (simd)" $ whnf Simd.encodeBase16SIMD bs25
         ]
       , bgroup "100" [ bench "base16-short" $ whnf BS16.encodeBase16' bs100L
         , bench "base16-bytestring" $ whnf Bos.encode bs100
         , bench "base16 (scalar)" $ whnf (assertBase16 . B16.encodeBase16_) bs100
-        , bench "base16 (simd)" $ whnf SIMD.encodeBase16SIMD bs100
+        , bench "base16 (simd)" $ whnf Simd.encodeBase16SIMD bs100
         ]
       , bgroup "1k" [ bench "base16-short" $ whnf BS16.encodeBase16' bs1kL
         , bench "base16-bytestring" $ whnf Bos.encode bs1k
         , bench "base16 (scalar)" $ whnf (assertBase16 . B16.encodeBase16_) bs1k
-        , bench "base16 (simd)" $ whnf SIMD.encodeBase16SIMD bs1k
+        , bench "base16 (simd)" $ whnf Simd.encodeBase16SIMD bs1k
         ]
       , bgroup "10k" [ bench "base16-short" $ whnf BS16.encodeBase16' bs10kL
         , bench "base16-bytestring" $ whnf Bos.encode bs10k
         , bench "base16 (scalar)" $ whnf (assertBase16 . B16.encodeBase16_) bs10k
-        , bench "base16 (simd)" $ whnf SIMD.encodeBase16SIMD bs10k
+        , bench "base16 (simd)" $ whnf Simd.encodeBase16SIMD bs10k
         ]
       , bgroup "100k" [ bench "base16-short" $ whnf BS16.encodeBase16' bs100kL
         , bench "base16-bytestring" $ whnf Bos.encode bs100k
         , bench "base16 (scalar)" $ whnf (assertBase16 . B16.encodeBase16_) bs100k
-        , bench "base16 (simd)" $ whnf SIMD.encodeBase16SIMD bs100k
+        , bench "base16 (simd)" $ whnf Simd.encodeBase16SIMD bs100k
         ]
       , bgroup "1mm" [ bench "base16-short" $ whnf BS16.encodeBase16' bs1mmL
         , bench "base16-bytestring" $ whnf Bos.encode bs1mm
         , bench "base16 (scalar)" $ whnf (assertBase16 . B16.encodeBase16_) bs1mm
-        , bench "base16 (simd)" $ whnf SIMD.encodeBase16SIMD bs1mm
+        , bench "base16 (simd)" $ whnf Simd.encodeBase16SIMD bs1mm
         ]
       ]
     , env bs' $ \ ~((bs25,bs100,bs1k,bs10k,bs100k,bs1mm),(bs25L,bs100L,bs1kL,bs10kL,bs100kL,bs1mmL)) ->
@@ -59,62 +59,62 @@ main =
       [ bgroup "25" [ bench "base16-short" $ whnf BS16.decodeBase16 bs25L
         , bench "base16-bytestring" $ whnf Bos.decode $ extractBase16 bs25
         , bench "base16 (scalar)" $ whnf B16.decodeBase16Typed_ bs25
-        , bench "base16 (simd)" $ whnf SIMD.decodeBase16SIMD bs25
+        , bench "base16 (simd)" $ whnf Simd.decodeBase16SIMD bs25
         , bench "base16-untyped (scalar)" $ whnf B16.decodeBase16_ $ extractBase16 bs25
-        , bench "base16-untyped (simd)" $ whnf SIMD.decodeBase16UntypedSIMD $ extractBase16 bs25
+        , bench "base16-untyped (simd)" $ whnf Simd.decodeBase16UntypedSIMD $ extractBase16 bs25
         , bench "isBase16" $ whnf B16.isBase16 $ extractBase16 bs25
         , bench "isValidBase16 (scalar)" $ whnf B16.isValidBase16 $ extractBase16 bs25
-        , bench "isValidBase16 (simd)" $ whnf SIMD.isValidBase16SIMD $ extractBase16 bs25
+        , bench "isValidBase16 (simd)" $ whnf Simd.isValidBase16SIMD $ extractBase16 bs25
         ]
       , bgroup "100" [ bench "base16-short" $ whnf BS16.decodeBase16 bs100L
         , bench "base16-bytestring" $ whnf Bos.decode $ extractBase16 bs100
         , bench "base16 (scalar)" $ whnf B16.decodeBase16Typed_ bs100
-        , bench "base16 (simd)" $ whnf SIMD.decodeBase16SIMD bs100
+        , bench "base16 (simd)" $ whnf Simd.decodeBase16SIMD bs100
         , bench "base16-untyped (scalar)" $ whnf B16.decodeBase16_ $ extractBase16 bs100
-        , bench "base16-untyped (simd)" $ whnf SIMD.decodeBase16UntypedSIMD $ extractBase16 bs100
+        , bench "base16-untyped (simd)" $ whnf Simd.decodeBase16UntypedSIMD $ extractBase16 bs100
         , bench "isBase16" $ whnf B16.isBase16 $ extractBase16 bs100
         , bench "isValidBase16 (scalar)" $ whnf B16.isValidBase16 $ extractBase16 bs100
-        , bench "isValidBase16 (simd)" $ whnf SIMD.isValidBase16SIMD $ extractBase16 bs100
+        , bench "isValidBase16 (simd)" $ whnf Simd.isValidBase16SIMD $ extractBase16 bs100
         ]
       , bgroup "1k" [ bench "base16-short" $ whnf BS16.decodeBase16 bs1kL
         , bench "base16-bytestring" $ whnf Bos.decode $ extractBase16 bs1k
         , bench "base16 (scalar)" $ whnf B16.decodeBase16Typed_ bs1k
-        , bench "base16 (simd)" $ whnf SIMD.decodeBase16SIMD bs1k
+        , bench "base16 (simd)" $ whnf Simd.decodeBase16SIMD bs1k
         , bench "base16-untyped (scalar)" $ whnf B16.decodeBase16_ $ extractBase16 bs1k
-        , bench "base16-untyped (simd)" $ whnf SIMD.decodeBase16UntypedSIMD $ extractBase16 bs1k
+        , bench "base16-untyped (simd)" $ whnf Simd.decodeBase16UntypedSIMD $ extractBase16 bs1k
         , bench "isBase16" $ whnf B16.isBase16 $ extractBase16 bs1k
         , bench "isValidBase16 (scalar)" $ whnf B16.isValidBase16 $ extractBase16 bs1k
-        , bench "isValidBase16 (simd)" $ whnf SIMD.isValidBase16SIMD $ extractBase16 bs1k
+        , bench "isValidBase16 (simd)" $ whnf Simd.isValidBase16SIMD $ extractBase16 bs1k
         ]
       , bgroup "10k" [ bench "base16-short" $ whnf BS16.decodeBase16 bs10kL
         , bench "base16-bytestring" $ whnf Bos.decode $ extractBase16 bs10k
         , bench "base16 (scalar)" $ whnf B16.decodeBase16Typed_ bs10k
-        , bench "base16 (simd)" $ whnf SIMD.decodeBase16SIMD bs10k
+        , bench "base16 (simd)" $ whnf Simd.decodeBase16SIMD bs10k
         , bench "base16-untyped (scalar)" $ whnf B16.decodeBase16_ $ extractBase16 bs10k
-        , bench "base16-untyped (simd)" $ whnf SIMD.decodeBase16UntypedSIMD $ extractBase16 bs10k
+        , bench "base16-untyped (simd)" $ whnf Simd.decodeBase16UntypedSIMD $ extractBase16 bs10k
         , bench "isBase16" $ whnf B16.isBase16 $ extractBase16 bs10k
         , bench "isValidBase16 (scalar)" $ whnf B16.isValidBase16 $ extractBase16 bs10k
-        , bench "isValidBase16 (simd)" $ whnf SIMD.isValidBase16SIMD $ extractBase16 bs10k
+        , bench "isValidBase16 (simd)" $ whnf Simd.isValidBase16SIMD $ extractBase16 bs10k
         ]
       , bgroup "100k" [ bench "base16-short" $ whnf BS16.decodeBase16 bs100kL
         , bench "base16-bytestring" $ whnf Bos.decode $ extractBase16 bs100k
         , bench "base16 (scalar)" $ whnf B16.decodeBase16Typed_ bs100k
-        , bench "base16 (simd)" $ whnf SIMD.decodeBase16SIMD bs100k
+        , bench "base16 (simd)" $ whnf Simd.decodeBase16SIMD bs100k
         , bench "base16-untyped (scalar)" $ whnf B16.decodeBase16_ $ extractBase16 bs100k
-        , bench "base16-untyped (simd)" $ whnf SIMD.decodeBase16UntypedSIMD $ extractBase16 bs100k
+        , bench "base16-untyped (simd)" $ whnf Simd.decodeBase16UntypedSIMD $ extractBase16 bs100k
         , bench "isBase16" $ whnf B16.isBase16 $ extractBase16 bs100k
         , bench "isValidBase16 (scalar)" $ whnf B16.isValidBase16 $ extractBase16 bs100k
-        , bench "isValidBase16 (simd)" $ whnf SIMD.isValidBase16SIMD $ extractBase16 bs100k
+        , bench "isValidBase16 (simd)" $ whnf Simd.isValidBase16SIMD $ extractBase16 bs100k
         ]
       , bgroup "1mm" [ bench "base16-short" $ whnf BS16.decodeBase16 bs1mmL
         , bench "base16-bytestring" $ whnf Bos.decode $ extractBase16 bs1mm
         , bench "base16 (scalar)" $ whnf B16.decodeBase16Typed_ bs1mm
-        , bench "base16 (simd)" $ whnf SIMD.decodeBase16SIMD bs1mm
+        , bench "base16 (simd)" $ whnf Simd.decodeBase16SIMD bs1mm
         , bench "base16-untyped (scalar)" $ whnf B16.decodeBase16_ $ extractBase16 bs1mm
-        , bench "base16-untyped (simd)" $ whnf SIMD.decodeBase16UntypedSIMD $ extractBase16 bs1mm
+        , bench "base16-untyped (simd)" $ whnf Simd.decodeBase16UntypedSIMD $ extractBase16 bs1mm
         , bench "isBase16" $ whnf B16.isBase16 $ extractBase16 bs1mm
         , bench "isValidBase16 (scalar)" $ whnf B16.isValidBase16 $ extractBase16 bs1mm
-        , bench "isValidBase16 (simd)" $ whnf SIMD.isValidBase16SIMD $ extractBase16 bs1mm
+        , bench "isValidBase16 (simd)" $ whnf Simd.isValidBase16SIMD $ extractBase16 bs1mm
         ]
       ]
     ]
